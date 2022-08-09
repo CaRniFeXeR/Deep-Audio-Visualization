899d3fdd0dc606b7df32c4f3f28c264a5bfd1f5d
Florian Kowarsch <florian.kowarsch@tuwien.ac.at>
2022-08-09 22:13:54 +0200

    added script to change sampling rate

---
+++ change_sampling_rate.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy.io import wavfile
+from scipy import interpolate
+
+NEW_SAMPLERATE = 44100
+
+old_samplerate, old_audio = wavfile.read("./tracks/Kodaline-Brother.wav")
+
+if old_samplerate != NEW_SAMPLERATE:
+    duration = old_audio.shape[0] / old_samplerate
+
+    time_old  = np.linspace(0, duration, old_audio.shape[0])
+    time_new  = np.linspace(0, duration, int(old_audio.shape[0] * NEW_SAMPLERATE / old_samplerate))
+
+    interpolator = interpolate.interp1d(time_old, old_audio.T)
+    new_audio = interpolator(time_new).T
+
+    wavfile.write("out.wav", NEW_SAMPLERATE, np.round(new_audio).astype(old_audio.dtype))
